520d8589b80cd3706a896be47a9b4e4ae1a92aed
MarcosEmilioV <MarcosEmilioV>
2025-04-14 16:12:32 -0600

    adaptive treshold

--- misc/ocr.py
+++ misc/ocr.py
@@ -7,18 +7,14 @@ text = pytesseract.image_to_string("perro.jpeg", config = '--psm 11 --oem 3 dawg
 print(text)
 
 def preprocess_image_for_ocr(image_path, output_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    clahe = cv2.createCLAHE(clipLimit =2.5, tileGridSize = (8,8))
-    contrast_enhanced = clahe.apply(gray)
-
-    kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
-    sharpened = cv2.filter2D(contrast_enhanced, -2, kernel)
-
     
+    image = cv2.imread(r'C:\Users\Lenovo\scannerapp\misc\portasdecente.jpeg')
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imwrite(output_path, sharpened)
-    return sharpened
+    something = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 30)
+
+    cv2.imwrite(output_path, something)
+    
+    return something
     
 preprocess_image_for_ocr("portasdecente.jpeg", "perro.jpeg")
--- misc/opencvtutorial/images.py
+++ misc/opencvtutorial/images.py
@@ -2,8 +2,11 @@ import cv2
 
 k_size = 15
 
-image = cv2.imread('noisy.png')
-grayscale = cv2.medianBlur(image, k_size)
+image = cv2.imread(r'C:\Users\Lenovo\scannerapp\misc\portasdecente.jpeg')
+grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+something = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 30)
 
-cv2.imshow('hi',grayscale)
+
+cv2.imshow('hi',image)
+cv2.imshow('processed', something)
 cv2.waitKey(0)
