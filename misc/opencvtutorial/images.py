26bfacaaa9dd6cb1227e0e9b7747e0d5899afb4c
MarcosEmilioV <MarcosEmilioV>
2025-04-21 11:14:48 -0600

    Edge Detection

--- misc/opencvtutorial/images.py
+++ misc/opencvtutorial/images.py
@@ -1,10 +1,17 @@
 import cv2
+import numpy as np
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd= 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+text = pytesseract.image_to_string(r"C:\Users\Lenovo\scannerapp\misc\perro.jpeg", config = '--psm 11 --oem 3 dawg')
+print(text)
 
 k_size = 15
 
 image = cv2.imread(r'C:\Users\Lenovo\scannerapp\misc\portasdecente.jpeg')
 grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-something = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 30)
+img_edge = cv2.Canny(grayscale, 150, 200)
+something = cv2.adaptiveThreshold(img_edge, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 30)
 
 
 cv2.imshow('hi',image)
