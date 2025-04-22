bea15aa18cdc374547b6c18bcd39c91a43536210
MarcosEmilioV <marcosemilio99@gmail.com>
2025-04-22 16:08:24 -0600

    preprocessing techniques

---
+++ misc/preprocess.py
@@ -0,0 +1,27 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd= 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+text = pytesseract.image_to_string("portastestingall.jpeg", config = '--psm 11 --oem 3 dawg')
+print(text)
+
+original = cv2.imread('portasdecente.jpeg')
+
+### Grayscale
+grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+
+###Binarization / Threshold
+binarized = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 40)
+
+###Invert
+inverted = cv2.bitwise_not(binarized)
+
+cv2.imwrite("portastestingall.jpeg", inverted)
+cv2.imshow('hi1',binarized)
+cv2.imshow('hi',inverted)
+
+
+
+
+
+cv2.waitKey(0)
