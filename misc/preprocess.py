13646a6d2321574c90b8632da89b4c19ace0eb94
MEVASQUEZ <marcosemilio99@gmail.com>
2025-07-08 17:36:24 -0600

    Best Result Yet: Inverted-Dilation

--- misc/preprocess.py
+++ misc/preprocess.py
@@ -1,6 +1,50 @@
 import cv2
 import pytesseract
 
+def getSkewAngle(cvImage) -> float:
+    # Prep image, copy, convert to gray scale, blur, and threshold
+    newImage = cvImage.copy()
+    gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Apply dilate to merge text into meaningful lines/paragraphs.
+    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
+    # But use smaller kernel on Y axis to separate between different blocks of text
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
+    dilate = cv2.dilate(thresh, kernel, iterations=2)
+
+    # Find all contours
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    for c in contours:
+        rect = cv2.boundingRect(c)
+        x,y,w,h = rect
+        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # Find largest contour and surround in min area box
+    largestContour = contours[0]
+    print (len(contours))
+    minAreaRect = cv2.minAreaRect(largestContour)
+    cv2.imwrite("temp/boxes.jpg", newImage)
+    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
+    angle = minAreaRect[-1]
+    if angle < -45:
+        angle = 90 + angle
+    return -1.0 * angle
+# Rotate the image around its center
+def rotateImage(cvImage, angle: float):
+    newImage = cvImage.copy()
+    (h, w) = newImage.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return newImage
+
+def deskew(cvImage):
+    angle = getSkewAngle(cvImage)
+    return rotateImage(cvImage, -1.0 * angle)
+
 def noise_removal(image): 
     import numpy as np
     kernel = np.ones((1, 1), np.uint8)
@@ -21,17 +65,26 @@ def thin_font(image):
 
 def thick_font(image): 
     import numpy as np
-    image = cv2.bitwise_not(image)
+    ##originally there was a bitwise not here
     kernel = np.ones((2,2), np.uint8)
     image = cv2.dilate(image, kernel, iterations=1)
     image = cv2.bitwise_not(image) ##reinverts the image to white background
     return image
 
+def remove_borders(image):
+    contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
+    cnt = cntsSorted[-1]
+    x, y, w, h = cv2.boundingRect(cnt)
+    crop = image[y:y+h, x:x+w]
+    return (crop)
+
 pytesseract.pytesseract.tesseract_cmd= 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-text = pytesseract.image_to_string("portastestingall.jpeg", config = '--psm 11 --oem 3 dawg')
+text = pytesseract.image_to_string("portasInverted_Dilated.jpeg", config = '--psm 11 --oem 3 dawg')
 print(text)
 
 original = cv2.imread('portasdecente.jpeg')
+testDeskew = cv2.imread("testDESKEW.png")
 
 ### Grayscale
 grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
@@ -39,8 +92,11 @@ grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 ###Binarization / Threshold
 binarized = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 40)
 
-###Invert
-inverted = cv2.bitwise_not(binarized)
+###No Borders
+no_borders = remove_borders(binarized) ##seems like it can only use a white backgroud image thats why binarized althought inverted better
+
+##inverted 
+inverted = cv2.bitwise_not(no_borders)
 
 ###Remove_noise
 no_noise = noise_removal(inverted)
@@ -51,15 +107,20 @@ eroded_image = thin_font(no_noise)
 ###Dilated image
 dilated_image = thick_font(no_noise)
 
+###Inverted-Dilation
+inverted_dilation = thick_font(inverted)
+
+###DESKEWED image
+fixed = deskew(testDeskew)
+
+cv2.imwrite("testDESKEW.jpeg", fixed)
 cv2.imwrite("portasDILATED.jpeg", dilated_image)
 cv2.imwrite("portasERODED.jpeg", eroded_image)
+cv2.imwrite("portas_binarized.jpeg", binarized)
+cv2.imwrite("portas_inverted.jpeg", inverted)
 cv2.imwrite("portas_noise.jpeg", no_noise)
-cv2.imwrite("portastestingall.jpeg", inverted)
-cv2.imshow('hi1',binarized)
-cv2.imshow('hellotest',inverted)
-cv2.imshow("noise", no_noise)
-cv2.imshow("erored", eroded_image)
-cv2.imshow("dilated", dilated_image)
+cv2.imwrite("portasNOBORDERS.jpeg", no_borders)
+cv2.imwrite("portasInverted_Dilated.jpeg", inverted_dilation)
 
 
 cv2.waitKey(0)
